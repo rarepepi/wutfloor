d5e53221696331f1b53f8ac351a4bd149f452323
pepimartinez <pepimartinez@protonmail.com>
2021-12-09 07:03:56 -0500

    new stuff

--- components/NFTAsset.js
+++ components/NFTAsset.js
@@ -6,10 +6,10 @@ const NFTAsset = (props) => {
         <div className="flex justify-center px-2">
             <Fade>
                 <div className="flex flex-col justify-center text-center text-white w-64 h-128 motion-safe:hover:scale-110 border-2 border-purple-400 rounded-xl my-8 mb-8 pt-8 pb-2">
-                    <p className="text-xs ">Total Supply <span className="text-green-200 block" >{Math.round(props.asset.total_supply * 100) / 100}</span></p>
-                    <p className="text-xs ">Owners <span className="text-green-200 block" >{Math.round(props.asset.num_owners * 100) / 100}</span></p>
-                    <p className="text-xs text-white">Market Cap <span className="block text-green-200" >Ξ{Math.round(props.asset.market_cap * 100) / 100}</span><span className="block text-green-200"  >(${(Math.round((props.asset.market_cap * props.ethPrice) * 100) / 100).toLocaleString()})</span></p>
-                    <p className="text-xs text-white">Total Volume <span className="block text-green-200"  >Ξ{Math.round(props.asset.total_volume * 100) / 100}</span><span className="block text-green-200"  >(${(Math.round((props.asset.total_volume * props.ethPrice) * 100) / 100).toLocaleString()})</span></p>
+                    <p className="text-xs ">Total Supply <span className="mb-2 text-green-200 block" >{Math.round(props.asset.total_supply * 100) / 100}</span></p>
+                    <p className="text-xs ">Owners <span className="mb-2 text-green-200 block" >{Math.round(props.asset.num_owners * 100) / 100}</span></p>
+                    <p className="text-xs text-white">Market Cap <span className=" block text-green-200" >Ξ{Math.round(props.asset.market_cap * 100) / 100}</span><span className="block text-green-200 mb-4"  >(${(Math.round((props.asset.market_cap * props.ethPrice) * 100) / 100).toLocaleString()})</span></p>
+                    <p className="text-xs text-white">Total Volume <span className="block text-green-200"  >Ξ{Math.round(props.asset.total_volume * 100) / 100}</span><span className="block text-green-200 mb-4"  >(${(Math.round((props.asset.total_volume * props.ethPrice) * 100) / 100).toLocaleString()})</span></p>
 
                     <img src={props.asset.image_url} className="w-36 h-36 mx-auto my-4 rounded-2xl p-2" />
                     <a href={url} target="_blank">

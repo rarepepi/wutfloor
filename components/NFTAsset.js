9dbe0d5ca00558ce5f08233d2126075d51c96ccf
pepimartinez <pepimartinez@protonmail.com>
2021-12-09 18:39:02 -0500

    mobile ui fixes

--- components/NFTAsset.js
+++ components/NFTAsset.js
@@ -9,7 +9,7 @@ const NFTAsset = (props) => {
         <div className="flex justify-center px-2 mb-16">
 
             <Fade>
-                <h1 className="text-white text-center -top-5 left-2 z-20 relative ring-2 ring-green-300 h-8 p-2 rounded-full ">{props.number + 1}</h1>
+                {/* <h1 className="text-white text-center -top-5 left-2 z-20 relative ring-2 ring-green-300 h-8 p-2 rounded-full ">{props.number + 1}</h1> */}
                 <div className="flex flex-col justify-center text-center text-white w-64 border-2 border-purple-500 rounded-xl lg:pt-8 pt-4">
                     <div className="cursor-pointer lg:w-48 lg:h-48 w-32 h-32 mx-auto mb-4">
                         <a href={url2} target="_blank">
--- pages/[address].js
+++ pages/[address].js
@@ -220,6 +220,9 @@ const Account = (props) => {
                         <TopNav />
                     </div>
                     <div className="flex flex-col justify-center">
+                        <h1 className="text-white font-bold my-16 p-4 text-4xl text-center" >
+                            <span className="text-purple-500">Wut</span>'s the <span className="text-purple-500">Floor </span>price?
+                        </h1>
                         <div className="shadow-2xl w-64 mx-auto items-center p-4">
                             <h1 className="text-white text-lg text-center block mt-2">Account</h1>
                             <h1 className="text-purple-500 text-center mt-2">{ethAddress.slice(0, 5) +
@@ -227,29 +230,30 @@ const Account = (props) => {
                                 ethAddress.slice(ethAddress.length - 4, ethAddress.length)}</h1>
 
                         </div>
-
-                        <div className="flex flew-row flex-wrap justify-evenly p-8 ">
-                            <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl items-center">
-                                <h1 className="text-white text-lg">ETH Price</h1>
-                                <p className=" text-green-400 mt-4">${ethPrice}</p>
-                            </div>
-                            <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl items-center">
-                                <h1 className=" text-white text-lg">Total Assets</h1>
-                                <p className="text-green-400 mt-4">{accountAssets.length}</p>
-                            </div>
-                            <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl">
-                                <h1 className=" text-white text-lg">Floor Value</h1>
-                                <div className="flex flex-row jusitfy-center text-center space-x-2">
-                                    <p className="text-green-400 mt-4">Ξ{Math.round(totalETHValue * 100) / 100}</p>
-                                    <p className="text-green-400 mt-4">(${(Math.round((totalETHValue * ethPrice) * 100) / 100).toLocaleString()})</p>
+                        <div className="">
+                            <div className="grid xl:grid-cols-4 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 p-8">
+                                <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl items-center">
+                                    <h1 className="text-white text-lg">ETH Price</h1>
+                                    <p className=" text-green-400 mt-4">${ethPrice}</p>
                                 </div>
+                                <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl items-center">
+                                    <h1 className=" text-white text-lg">Total Assets</h1>
+                                    <p className="text-green-400 mt-4">{accountAssets.length}</p>
+                                </div>
+                                <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl">
+                                    <h1 className=" text-white text-lg">Floor Value</h1>
+                                    <div className="flex flex-col jusitfy-center text-center space-x-2">
+                                        <p className="text-green-400 mt-4"><img src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg" className="h-6 w-6 inline mr-2" />{Math.round(totalETHValue * 100) / 100}</p>
+                                        <p className="text-green-400 mt-4">(${(Math.round((totalETHValue * ethPrice) * 100) / 100).toLocaleString()})</p>
+                                    </div>
 
-                            </div>
-                            <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl">
-                                <h1 className="text-white text-lg">7 Day Avg</h1>
-                                <div className="flex flex-row jusitfy-center text-center space-x-2">
-                                    <p className="text-green-400 mt-4">Ξ{Math.round(totalETHValue7Day * 100) / 100}</p>
-                                    <p className="text-green-400 mt-4">(${(Math.round((totalETHValue7Day * ethPrice) * 100) / 100).toLocaleString()})</p>
+                                </div>
+                                <div className="flex flex-col justify-center text-center lg:p-10 p-8 shadow-2xl">
+                                    <h1 className="text-white text-lg">7 Day Avg</h1>
+                                    <div className="flex flex-col jusitfy-center text-center space-x-2">
+                                        <p className="text-green-400 mt-4"><img src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg" className="h-6 w-6 inline mr-2" />{Math.round(totalETHValue7Day * 100) / 100}</p>
+                                        <p className="text-green-400 mt-4">(${(Math.round((totalETHValue7Day * ethPrice) * 100) / 100).toLocaleString()})</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
